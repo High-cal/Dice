935ee3d3fcd6837e7f113a6d3b3e920588b8ed9d
Hical <h41kal.pr@gmail.com>
2025-03-15 13:57:49 +0700

    finished

--- dices.py
+++ dices.py
@@ -1,2 +1,15 @@
 import random
 
+dice_types = {
+        "d6": [1, 2, 3, 4, 5, 6],
+        "d10": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "d20": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+}
+
+dice = input("Which dice do you wanna roll (d6, d10, d20): ").lower().strip()
+
+if dice in dice_types:
+    result = random.choice(dice_types[dice])
+    print(f"You Rolled: {result}")
+else:
+    print("Invalid dice type. Please choose from d6, d10, d20")
